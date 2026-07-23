a7161f3799341189f1cc8c1482424750c4a2fa69
Joseph Quested <josephquested@gmail.com>
2026-07-23 20:33:14 +1200

    merge

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
@@ -7,25 +8,58 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        
+        timeRemaining = gameTime;
+        UpdateTimerDisplay();
     }
 
     void Update()
     {
+        UpdateTimer();
         CheckForRestart();
     }
 
     // -- GAME STATE -- //
 
     public GameObject gameOverScreen;
+    public TextMeshProUGUI timerText;
+    public float gameTime = 60f;
 
     bool isGameOver = false;
+    float timeRemaining;
+    float timerTickCounter;
 
     public void GameOver()
     {
         isGameOver = true;
         gameOverScreen.SetActive(true);
         Debug.Log("Game Over!");
+    }
+
+    void UpdateTimer()
+    {
+        if (isGameOver)
+            return;
+
+        timerTickCounter += Time.deltaTime;
+        if (timerTickCounter >= 1f)
+        {
+            timeRemaining -= 1f;
+            timerTickCounter = 0f;
+            UpdateTimerDisplay();
+            
+            if (timeRemaining <= 0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    void UpdateTimerDisplay()
+    {
+        if (timerText != null)
+        {
+            timerText.text = Mathf.Max(0, Mathf.FloorToInt(timeRemaining)).ToString();
+        }
     }
 
     void CheckForRestart()
